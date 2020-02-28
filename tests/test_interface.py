847735ab9552c3bc9ea9a3cc665fa844e8ca145f
Matthew Spera <speramatthew@gmail.com>
2020-02-28 16:49:59 +1100

    continued unittest development

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -6,11 +6,11 @@ from pantest.interface import PanApi, PanCli, PanHybrid
 
 def setUpModule():
     working_dir = os.path.dirname(__file__)
-    file_path = os.path.join(working_dir, 'etc/responses.yml')
+    file_path = os.path.join(working_dir, 'etc/test_vars.yml')
 
-    global expected_responses
+    global test_vars
     with open(file_path) as file_obj:
-        expected_responses = yaml.safe_load(file_obj)
+        test_vars = yaml.safe_load(file_obj)
 
     global device_info
     device_info = {
@@ -27,29 +27,33 @@ class TestPanApi(unittest.TestCase):
 
     def test_get_system_info(self):
         output = self.api.get_system_info()
-        self.assertEqual(expected_responses['hostname'], output['system']['hostname'])
+        self.assertEqual(test_vars['hostname'], output['system']['hostname'])
 
     def test_get_route_table(self):
         output = self.api.get_route_table()
-        self.assertTrue(next((entry for entry in output['entry'] if entry['destination'] == expected_responses['test_route_dest'])), None)
+        self.assertTrue(next((entry for entry in output['entry'] if entry['destination'] == test_vars['test_route_dest'])), None)
 
     def test_get_route_interface(self):
         output = self.api.get_route_interface()
-        self.assertTrue(next((i for i in output['interface'] if i['name'] == expected_responses['host_only_route_int'])), None)
+        self.assertTrue(next((i for i in output['interface'] if i['name'] == test_vars['host_only_route_int'])), None)
 
     def test_get_system_environmentals(self):
         pass
 
     def test_get_ntp(self):
         output = self.api.get_ntp()
-        self.assertEqual(expected_responses['ntp_status'], output['synched'])
+        self.assertEqual(test_vars['ntp_status'], output['synched'])
 
     def test_get_panorama_status(self):
         pass
 
+    def test_get_ha_status(self):
+        output = self.api.get_ha_status()
+        self.assertEqual(test_vars['ha_enabled_status'], output['enabled'])
+
     def test_get_interface_hardware(self):
         output = self.api.get_interface_hardware()
-        self.assertTrue(next((i for i in output['hw']['entry'] if i['mac'] == expected_responses['interface_mac'])), None)
+        self.assertTrue(next((i for i in output['hw']['entry'] if i['mac'] == test_vars['interface_mac'])), None)
 
 class TestPanCli(unittest.TestCase):
 
@@ -62,7 +66,7 @@ class TestPanCli(unittest.TestCase):
 
     def test_enter_config_mode(self):
         output = self.cli.enter_config_mode()
-        self.assertIn(expected_responses['config_mode_prompt'], output)
+        self.assertIn(test_vars['config_mode_prompt'], output)
 
     def tearDown(self):
         del self.cli
@@ -75,7 +79,7 @@ class TestPanHybrid(unittest.TestCase):
 
     def test_get_connectivity(self):
         output = self.api_cli.get_connectivity()
-        self.assertEqual(expected_responses['packet_loss'], output['192.168.56.2'])
+        self.assertEqual(test_vars['packet_loss'], output['192.168.56.2'])
 
 if __name__ == "__main__":
     unittest.main()
---
+++ tests/test_testcases.py
@@ -0,0 +1,115 @@
+import os
+import yaml
+import unittest
+
+from pantest.testcases import PanoramaTestCases, FirewallTestCases, GeneralTestCases
+
+def setUpModule():
+    working_dir = os.path.dirname(__file__)
+    file_path = os.path.join(working_dir, 'etc/test_vars.yml')
+
+    global test_vars
+    with open(file_path) as file_obj:
+        test_vars = yaml.safe_load(file_obj)
+
+    global device_info
+    device_info = {
+        'ip' : os.environ.get('TEST_IP'),
+        'username' : os.environ.get('TEST_USERNAME'),
+        'password' : os.environ.get('TEST_PASSWORD')
+    }
+
+class TestPanoramaTestCases(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+class TestFirewallTestCases(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.fw_tester = FirewallTestCases(device_info)
+
+    def test_t_system_env_alarms_fw(self):
+        pass
+
+    def test_t_connectivity(self):
+        pass
+
+    def test_t_panorama_connected(self):
+        pass
+
+    def test_t_ha_peer_up(self):
+        output_1 = self.fw_tester.t_ha_peer_up(test_vars['ha_peer_up_status'])
+        output_2 = self.fw_tester.t_ha_peer_up(test_vars['not_ha_peer_up_status'])
+        self.assertTrue(output_1['result'])
+        self.assertFalse(output_2['result'])
+        self.assertEqual(test_vars['ha1_status'], output_2['info']['ha1-link-status'])
+        self.assertEqual(test_vars['ha2_status'], output_2['info']['ha2-link-status'])   
+
+    def test_t_ha_match(self):
+        pass
+
+    def test_t_ha_config_synced(self):
+        output_1 = self.fw_tester.t_ha_config_synced(test_vars['ha_config_sync_status'])
+        output_2 = self.fw_tester.t_ha_config_synced(test_vars['not_ha_config_sync_status'])
+        self.assertTrue(output_1['result'])
+        self.assertFalse(output_2['result'])
+        self.assertEqual(test_vars['ha_config_sync_status'], output_2['info']['ha-config-sync-status'])
+
+    def test_t_interfaces_up(self):
+        pass
+
+    def test_t_traffic_log_forward(self):
+        pass
+
+class TestGeneralTestCases(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.gen_tester = GeneralTestCases(device_info)
+
+    def test_t_system_version(self):
+        output_1 = self.gen_tester.t_system_version(test_vars['sys_version'])
+        output_2 = self.gen_tester.t_system_version(test_vars['not_sys_version'])
+        self.assertTrue(output_1['result'])
+        self.assertFalse(output_2['result'])
+        self.assertEqual(output_2['info']['current-sw-version'], test_vars['sys_version'])
+        self.assertEqual(output_2['info']['target-sw-version'], test_vars['not_sys_version'])
+
+    def test_t_config_diff(self):
+        working_dir = os.path.dirname(__file__)
+
+        file_path_1 = os.path.join(working_dir, test_vars['config_set'])
+
+        with open(file_path_1, 'r') as file_obj:
+            config_set_list = file_obj.read().splitlines()
+            output_1 = self.gen_tester.t_config_diff(config_set_list)
+
+        file_path_2 = os.path.join(working_dir, test_vars['not_config_set'])
+
+        with open(file_path_2, 'r') as file_obj:
+            config_set_list = file_obj.read().splitlines()
+            output_2 = self.gen_tester.t_config_diff(config_set_list)
+
+        self.assertTrue(output_1['result'])
+        self.assertFalse(output_2['result'])
+        self.assertListEqual(test_vars['removed_config'], output_2['info']['config-changes']['removed'])
+        self.assertListEqual(test_vars['added_config'], output_2['info']['config-changes']['added'])          
+
+
+    def test_t_ha_enabled(self):
+        output_1 = self.gen_tester.t_ha_enabled(test_vars['ha_enabled_status'])
+        output_2 = self.gen_tester.t_ha_enabled(test_vars['not_ha_enabled_status'])
+        self.assertTrue(output_1['result'])
+        self.assertFalse(output_2['result'])
+
+    def test_t_ntp_synced(self):
+        output_1 = self.gen_tester.t_ntp_synced(test_vars['ntp_status'])
+        output_2 = self.gen_tester.t_ntp_synced(test_vars['not_ntp_status'])
+        self.assertTrue(output_1['result'])
+        self.assertFalse(output_2['result'])
+
+if __name__ == "__main__":
+    unittest.main()
